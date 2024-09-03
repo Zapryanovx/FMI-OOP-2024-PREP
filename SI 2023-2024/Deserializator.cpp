0c099eee36c6259b10cec2bd24dc1168d390ff81
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2024-09-03 05:10:45 +0300

    Delete SI 2023-2024 directory

--- SI 2023-2024/Deserializator.cpp
+++
@@ -1,8 +0,0 @@
-#include "Deserializator.h"
-#pragma warning(disable: 4996)
-
-Deserializator::Deserializator(const char* str)
-{
-	std::strcpy(fileName, str);
-}
-
--- SI 2023-2024/Serializator.cpp
+++
@@ -1,8 +0,0 @@
-#include "Serializator.h"
-#pragma warning(disable: 4996)
-
-Serializator::Serializator(const char* str, const HexArray& arr) : arr(arr)
-{
-	std::strcpy(fileName, str);
-}
-
